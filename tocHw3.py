5edac782a49e29a5bb3542edb37fd22e31bf64f1
Homer Chen <xxhomey19@gmail.com>
2015-06-14 21:13:34 +0800

    Update Time : 2015/6/14

--- tocHw3.py
+++ tocHw3.py
@@ -1,7 +1,7 @@
 import re
 import sys
-#import time
-#tStart = time.time()
+import time
+tStart = time.time()
 
 infile = open(sys.argv[1], 'r')
 topk = int(sys.argv[2])
@@ -12,51 +12,23 @@ for line in infile:
 	url = re.findall('"WARC-Target-URI":"([^"]*)"', line) 
 	str_url = ''.join(url)
 
-	links = re.findall('"Links":(\[(\{([^\}])*\})(\,\{[^\}]*\})*([^\]]*)\])', line)
+	links = re.findall('"Links"[ :]+(\[[^]]*\])', line)
 
 	str_links = ''.join(str(i) for i in links)
 
-	href = re.findall('"href":+\"http[s]?://([^"]*\")', str_links)
+	href = re.findall('"href"[ :]+\"[^"]*\"', str_links)
 	num_href = len(href)
 
-	url = re.findall('"url":+\"http[s]?://([^"]*\")', str_links)
+	url = re.findall('"url"[ :]+\"[^"]*\"', str_links)
 	num_url = len(url)
 
 	total = num_href + num_url
 
 	total_list.append(total)
 	dict1.setdefault(total, []).append(str_url) #dict with list
-
-def merge_sort(m):
-   	if len(m) <= 1:
-        		return m
- 
-   	middle = len(m) / 2
-    	left = m[:middle] #above middle
-   	right = m[middle:] #behind middle
- 
-   	left = merge_sort(left)
-   	right = merge_sort(right)
-   	return list(merge(left, right))
-
-def merge(left, right):
-	result = []
-	left_idx, right_idx = 0, 0
-	while left_idx < len(left) and right_idx < len(right):
-		if left[left_idx] >= right[right_idx]:
-			result.append(left[left_idx])
-			left_idx += 1
-		else:
-			result.append(right[right_idx])
-			right_idx += 1
-	if left:
-		result.extend(left[left_idx:])
-	if right:
-		result.extend(right[right_idx:])
-	return result
-
 #print total_list
-total_list = merge_sort(total_list) #do merge_sort
+#total_list = merge_sort(total_list) #do merge_sort
+total_list.sort(reverse=True) #do merge_sort
 
 count = 0
 for i in range(0, topk):
@@ -71,5 +43,5 @@ for i in range(0, topk):
 	count += len(tmplist) 
 	if count >= topk: #in case lenth of list exceed topk
 		break
-#tEnd = time.time()
-#print "It cost %f sec" % (tEnd - tStart)
+tEnd = time.time()
+print "It cost %f sec" % (tEnd - tStart)
