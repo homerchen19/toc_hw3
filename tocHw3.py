d4a6f66e6cc83a461c875ed24efa295efff83f10
Homer Chen <xxhomey19@gmail.com>
2015-06-15 09:49:38 +0800

    Update Time : 2015/6/15

--- tocHw3.py
+++ tocHw3.py
@@ -1,6 +1,10 @@
 import re
 import sys
 import os.path
+#import time
+#tStart = time.time()
+
+#f = open('outfile.txt', 'w')
 
 def verify_inputfile():
 	inputfile = sys.argv[1]
@@ -15,57 +19,61 @@ def verify_inputfile():
 		
 	return inputfile
 
-def verify_query():
+def verify_topk():
 	if len(sys.argv) > 2:
-		query = sys.argv[2]
+		topk = int(sys.argv[2])
 	else:
-		print "There is no query"
+		print "There is no topk !"
 		sys.exit(0)
-	return query
+
+	if topk < 0:
+		print "The topk should be positive !"
+		sys.exit(0)
+		
+	return topk
 
 valid_infile = verify_inputfile()
 infile = open(valid_infile, 'r')
-query = verify_query()
+topk = verify_topk()
 
-nofound1, nofound2 = 0, 0
-typeNum = {}
+total_list= []
+dict1 = {}
 
-def findtype(list_tmp):
-	global nofound1, nofound2, query
-	global typeNum
-	for item in list_tmp: #item = each elements in list_href. Its type is string
-		if(item.find(query) < 0): # if item doesn't contain query
-			item = re.findall('[.]([\w]+)[\?|"]', item) #parse the last type
-			if item:
-				nofound2 = 1;
-				tmp = ''.join(item)
-				#print tmp
-				if tmp in typeNum:
-					typeNum[tmp] += 1
-				else:
-					typeNum[tmp] = 1
-				#print typeNum
 for line in infile:
 	url = re.findall('"WARC-Target-URI":"([^"]*)"', line) 
 	str_url = ''.join(url)
 
-	if (str_url.find(query) >= 0): # compare web pages & query
-		nofound1 = 1
+	links = re.findall('"Links":\[(.+)\](,"Head"|\},"Entity-Digest")', line)
+	str_links = ''.join(str(i) for i in links)
 
-		links = re.findall('"Links":\[(.+)\](,"Head"|\},"Entity-Digest")', line)
-		#print "len = " + str(len(links))
-		str_links = ''.join(str(i) for i in links)
+	href = re.findall('"href"[ :]+\"([^"]*)\"', str_links)
+	num_href = len(href)
 
-		list_href = re.findall('"href":+\"http[s]?://([^"]*\")', str_links) #parse 'http(s):'
-		findtype(list_href)
+	url = re.findall('"url"[ :]+\"([^"]*)\"', str_links)
+	num_url = len(url)
 
-		list_url = re.findall('"url":+\"http[s]?://([^"]*\")', str_links)
-		findtype(list_url)
+	total = num_href + num_url
 
-if nofound1 == 0:
-	print'Page not found!'
-elif nofound2 == 0:
-	print'Type not found!'
-else:
-	for x in typeNum:
-		print x, ':' ,typeNum[x]
+	total_list.append(total)
+	dict1.setdefault(total, []).append(str_url) #dict with list
+#print total_list
+#total_list = merge_sort(total_list) #do merge_sort
+total_list.sort(reverse=True) #do merge_sort
+
+count = 0
+for i in range(0, topk):
+	tmplist = dict1.get(total_list[i]) #find key in dict1
+	if tmplist == None:
+		continue
+	for item in tmplist:
+		if item != "":
+			#f.write( item + ":" + str(total_list[i]) + "\n") #print list
+			print item + ":" + str(total_list[i]) #print list
+
+	dict1.pop(total_list[i], None) #pop the key(= total_list[i])
+	count += len(tmplist) 
+	if count >= topk: #in case lenth of list exceed topk
+		break
+#f.close()
+#tEnd = time.time()
+#print "It cost %f sec" % (tEnd - tStart)
